cf10667a2ca5765f789cd540271934276d2e7178
Pavlo Kravets <60813108+Legamanchik@users.noreply.github.com>
2023-10-04 00:19:30 +0300

    Add files via upload
    Task 4 on bash

---
+++ Task_4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while getopts "p:t:" flag; do
+    case "${flag}" in
+        p) path="${OPTARG}" ;;
+        t) text="${OPTARG}" ;;
+    esac
+done
+
+if [ $# -eq 0 ]; then
+    echo "Usage: Enter first param(-p) - path, then second param(-t) text"
+else
+    if [ -z "$path" ]; then
+        echo "Error: -p path is empty"
+        exit 1
+    elif [ ! -e "$path" ]; then
+        echo "Error: $path path does not exist"
+        exit 1
+    elif [ -z "$text" ]; then``
+        echo "Error: -t text is empty"
+        exit 1
+        
+    else
+        grep -R "$text" "$path"
+    fi
+fi
