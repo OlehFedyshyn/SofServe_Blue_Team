67fd3aabd0ea303ad016967d0bd97d13a84a4774
Denis Nasonov <dnason777@gmail.com>
2023-10-04 15:03:55 +0300

    Task 3 on Python realizataion

--- task3.py
+++ task3.py
@@ -4,7 +4,7 @@ import sys
 def count_files_by_extension(directory_path, extension):
     try:
         count = 0
-        for root, dirs, files in os.walk(directory_path):
+        for _, _, files in os.walk(directory_path):
             for file in files:
                 if file.endswith('.' + extension):
                     count += 1
@@ -14,14 +14,19 @@ def count_files_by_extension(directory_path, extension):
         return -1  
     
 if __name__ == "__main__":
+
     if len(sys.argv) < 3:
         print('Usage: python task3.py <directory_path> <extension>')
     elif len(sys.argv) > 3: 
         print('You should use 2 arguments')
+    elif not os.path.isdir(sys.argv[1]):
+            print(f'There is no such directory as you entered {sys.argv[1]}')
+        
     else:   
         directory_path = sys.argv[1]
         extension = sys.argv[2]
         file_count = count_files_by_extension(directory_path, extension)
+        
         if file_count >= 0:
             print(f'Number of files with extension .{extension} in folder {directory_path}: {file_count} files')
             
