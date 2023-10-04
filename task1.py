05031646cf9854d6988bfcfeaad9af45ad03dce0
AndriyGalias <agalias.softserve@gmail.com>
2023-10-04 18:11:34 +0300

    Corrected exception handler

--- task1.py
+++ task1.py
@@ -2,23 +2,29 @@ import os
 import sys
 import fnmatch
 
-# Handler for invalid inputs
-if len(sys.argv) != 3:
-    print("Usage: python3 task1.py <path to directory> <file extension>")
-elif not os.path.isdir(sys.argv[1]):
-    print(f"Directory '{sys.argv[1]}' does not exists. Provide a valid path to directory")
+try:
+    if len(sys.argv) != 3:
+        raise ValueError("Usage: python3 task1.py <path to directory> <file extension>")
+    elif not os.path.isdir(sys.argv[1]):
+        raise ValueError(f"Directory '{sys.argv[1]}' does not exist. Provide a valid path to a directory")
 
-# Find files
-file_list = []
-for root, directories, files in os.walk(sys.argv[1]):
-    for file in files:  
-        if fnmatch.fnmatch(file, f"*.{sys.argv[2]}"):
-            file_list.append(os.path.join(root, file))
+    # Find files
+    file_list = []
+    for root, directories, files in os.walk(sys.argv[1]):
+        for file in files:
+            if fnmatch.fnmatch(file, f"*.{sys.argv[2]}"):  # Use sys.argv[2] for the file extension
+                file_list.append(os.path.join(root, file))
 
-# Check list and print files if there is
-if file_list:
-    print("Founded files: ")
-    for file_path in file_list:
-        print(file_path)
-else: 
-    print(f"No files with specified '{sys.argv[2]}' extension found in '{sys.argv[1]}'")
+    # Check list and print files if there are any
+    if file_list:
+        print("Found files:")
+        for file_path in file_list:
+            print(file_path)
+    else: 
+        print(f"No files with specified '{sys.argv[2]}' extension found in '{sys.argv[1]}'")
+
+except ValueError as err:
+    print(err)
+
+
+
